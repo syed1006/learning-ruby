af8c2fe7a0a06e8859916dcbacde7eec92e64a78
syedgaian <syedgaian@users.noreply.github.com>
2026-06-22 18:16:27 +0530

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  # validates :username, presence: true, length: { minimum: 6, maximum: 100 }
+  # validates :email, presence: true, length: { minimum: 10, maximum: 300 }
+end
