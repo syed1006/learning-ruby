782136f5cb876cb55369ce65b98c922c2ff3ad31
syedgaian <syedgaian@users.noreply.github.com>
2026-06-22 18:13:25 +0530

    added users tab;e

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_18_102340) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_22_124219) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 end
