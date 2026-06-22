b328250078d973cd9dba6f3a22783b8423c7f50e
syedgaian <syedgaian@users.noreply.github.com>
2026-06-22 11:58:01 +0530

    added new endpoint

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    puts "NEW ACTION CALLED"
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      puts "CREATE ACTION ERRORS: #{@article.errors.full_messages}"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
