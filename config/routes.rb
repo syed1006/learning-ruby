098caac7435dceb053ac4dff69fae60f7cac1370
syedgaian <syedgaian@users.noreply.github.com>
2026-06-23 17:04:24 +0530

    added user signup

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
 
   # resources :articles, only: [ :show, :index, :new, :create, :edit, :update, :destroy ]
   resources :articles
+
+  get "signup", to: "users#new"
+  resources :users, except: [ :new ]
 end
