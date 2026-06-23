a5ca8e0a8809992ed694459a9002fe46a2acc622
syedgaian <syedgaian@users.noreply.github.com>
2026-06-23 13:07:59 +0530

    association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_22_124219) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_23_063856) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
