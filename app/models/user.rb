1a4a5bfcf54d729e8d71de49a6229fda75de490c
syedgaian <syedgaian@users.noreply.github.com>
2026-06-23 16:46:25 +0530

    added auth setup with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_23_063856) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_23_110910) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_23_063856) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.string "username"
   end
