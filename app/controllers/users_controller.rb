b6490139cec3148f6dfe5b8aee69d383ede87ac4
syedgaian <syedgaian@users.noreply.github.com>
2026-06-23 17:13:23 +0530

    lol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog, You have signed up successfully."
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
